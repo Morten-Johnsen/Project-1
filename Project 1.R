703472e5ab7279ed57215d0797dbc3f7286a5645
Morten-Johnsen <s184334@dtu.dk>
2022-09-03 22:06:22 +0200

    Update Project 1.R
    test changes

--- Project 1.R
+++ Project 1.R
@@ -1,3 +1,12 @@
-print('Hello world')
-print('Test 1')
-print('Test 2')
+rm(list = ls())
+library(tidyverse)
+library(reshape)
+
+
+test.data <- data.frame("id" = sample(c("1","2","3","4"), size = 100, replace = T, prob = c(0.1,0.4,0.2,0.3))
+                        ,"N" = rnorm(100))
+ggplot(test.data)+
+  geom_histogram(aes(x = N, fill = id), colour = "white")+
+  theme_linedraw()+
+  labs(x = "", y = "", fill = "ID")+
+  ggtitle("Test hist")
